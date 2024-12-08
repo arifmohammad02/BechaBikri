cca4a698a6a584cb4636762824e3c70c323ec008
Creative-Md-Arif <mohammadarif.ma02@gmail.com>
2024-12-09 00:56:24 +0600

    Updated

--- frontend/src/components/OrderNowButton.jsx
+++ frontend/src/components/OrderNowButton.jsx
@@ -1,29 +1,29 @@
-/* eslint-disable react/prop-types */
-import { useDispatch } from "react-redux";
-import { addToCart } from "../redux/features/cart/cartSlice";
-import { useNavigate } from "react-router-dom";
+// /* eslint-disable react/prop-types */
+// import { useDispatch } from "react-redux";
+// import { addToCart } from "../redux/features/cart/cartSlice";
+// import { useNavigate } from "react-router-dom";
 
-const OrderNowButton = ({ product, qty = 1, customStyles = "" }) => {
-  const dispatch = useDispatch();
-  const navigate = useNavigate();
+// const OrderNowButton = ({ product, qty = 1, customStyles = "" }) => {
+//   const dispatch = useDispatch();
+//   const navigate = useNavigate();
 
-  const handleOrderNow = () => {
-    // Add product to cart and navigate to the cart page
-    dispatch(addToCart({ ...product, qty }));
-    navigate("/cart");
-  };
+//   const handleOrderNow = () => {
+//     // Add product to cart and navigate to the cart page
+//     dispatch(addToCart({ ...product, qty }));
+//     navigate("/cart");
+//   };
 
-  return (
-    <button
-      onClick={handleOrderNow}
-      disabled={product.countInStock === 0} // Disable if the product is out of stock
-      className={`text-white bg-red-600 hover:bg-red-700 focus:outline-none 
-        font-medium rounded-md text-sm px-4 h-10 dark:bg-red-600 
-        dark:hover:bg-red-700 dark:focus:ring-red-800 ${customStyles}`}
-    >
-      Order Now
-    </button>
-  );
-};
+//   return (
+//     <button
+//       onClick={handleOrderNow}
+//       disabled={product.countInStock === 0} // Disable if the product is out of stock
+//       className={`text-white bg-red-600 hover:bg-red-700 focus:outline-none 
+//         font-medium rounded-md text-sm px-4 h-10 dark:bg-red-600 
+//         dark:hover:bg-red-700 dark:focus:ring-red-800 ${customStyles}`}
+//     >
+//       Order Now
+//     </button>
+//   );
+// };
 
-export default OrderNowButton;
+// export default OrderNowButton;
