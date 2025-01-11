d1b904aae4b5bc6f18040053a8602575a98f5536
unknown <abdullahabirweb10@gmail.com>
2025-01-11 18:12:52 +0600

    whatsapp

--- frontend/src/components/Whatsapp.jsx
+++ frontend/src/components/Whatsapp.jsx
@@ -8,7 +8,7 @@ const Whatsapp = () => {
         href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css"
       />
       <a
-        href="https://api.whatsapp.com/send?phone=+8801793-766634&text=Hola%21%20Quisiera%20m%C3%A1s%20informaci%C3%B3n%20sobre%20Varela%202."
+        href="https://api.whatsapp.com/send?phone=+8801793-766634&text=Hello%21%20Welcome%20E%C3%OnlineSolution%20Howcanihelpi%C3%B3n%20You%20Sir?%202."
         class="float"
         target="_blank"
       >
